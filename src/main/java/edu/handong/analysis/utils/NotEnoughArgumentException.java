aea056bc274bc78567e14952e3110755cbd3f5e1
동준 <@DESKTOP-FS8K790>
2019-05-23 19:38:08 +0900

    commit

--- src/main/java/edu/handong/analysis/utils/NotEnoughArgumentException.java
+++ src/main/java/edu/handong/analysis/utils/NotEnoughArgumentException.java
@@ -1,14 +1,16 @@
 package edu.handong.analysis.utils;
 
-public class NotEnoughArgumentException 
+public class NotEnoughArgumentException extends Exception
 {
-	public NotEnoughArgumentException()
-	{
-		
-	}
+	{	
+		public NotEnoughArgumentException()
+		{
+			super("No CLI argument Exception! Please put a file path.");
+		}		
 	
-	public NotEnoughArgumentException(String message)
-	{
-		
+		public NotEnoughArgumentException(String message)
+		{
+			super(message);
+		}
 	}
 }
