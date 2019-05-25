8f1ba36c1592eec41b5611d086428c2a4295d984
동준 <@DESKTOP-FS8K790>
2019-05-25 16:01:04 +0900

    commit

--- src/main/java/edu/handong/analysise/utils/Utils.java
+++ src/main/java/edu/handong/analysise/utils/Utils.java
@@ -1,40 +1,41 @@
 package edu.handong.analysise.utils;
 
+import java.util.ArrayList;
 import java.io.*;
+import java.util.Scanner;
 
 public class Utils 
 {
 	public static ArrayList<String> getLines(String file,boolean removeHeader)
 	{
-		File csv = new File("hw5data.csv");
-		
-        BufferedReader br = new BufferedReader(new FileReader(csv));
-        String line = "";
-        int row = 0 ,i;
-
-        while ((line = br.readLine()) != null) 
-        {
-            // -1 옵션은 마지막 "," 이후 빈 공백도 읽기 위한 옵션
-        	String[][] arr = new String[100][100]; 
-            String[] token = line.split(",", -1);
-            for(i=0 ; i<6 ; i++) 
-            {
-            	arr[row][i] = token[i];
-            }
-            
-            // CSV에서 읽어 배열에 옮긴 자료 확인하기 위한 출력
-            for(i=0 ; i<6 ; i++) 
-            {
-            	System.out.print(arr[row][i] + ",");
-            }
-            System.out.println("");
-            
-            row++;
-        }
-        br.close();
-		//파일을 읽어들이는 부분
+		ArrayList<String> Line = new ArrayList<String>();
+		try
+		{
+			Scanner in = new Scanner(new File(file));
+			String temp;
+			
+			while(true)
+			{
+				if (in.hasNextLine() == false)
+					break;
+				temp = in.nextLine();
+				
+				if (removeHeader == true)
+				{
+					removeHeader = false;
+					continue;
+				}
+				
+				Line.add(temp);			
+			} // while end;
+			
+		} catch(Exception e)
+		{
+			System.out.println("Plz re input file name");
+		}
+		return Line;
 	}
-	
+
 	public static void writeAFile(ArrayList<String> lines, String targetFileName)
 	{
 		
