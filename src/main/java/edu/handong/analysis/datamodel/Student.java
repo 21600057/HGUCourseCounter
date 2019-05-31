cd89e838d78140a5ee01367b4a53a6cc20564817
동준 <@DESKTOP-FS8K790>
2019-05-31 23:02:50 +0900

    commit

--- src/main/java/edu/handong/analysis/datamodel/Student.java
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -6,29 +6,48 @@ import java.util.HashMap;
 public class Student 
 {
 	private String studentId;
-	private ArrayList<Course> coursesTaken;
+	private ArrayList<Course> coursesTaken=new ArrayList<Course>();
 	private HashMap<String,Integer> semestersByYearAndSemester;
 
 	public Student(String studentId)
 	{
-		
+		this.studentId = studentId;
 	}
 	
 	public void addCourse(Course newRecord)
 	{
-		
+		coursesTaken.add(newRecord);
 	}
 	
 	public HashMap<String,Integer> getSemestersByYearAndSemester()
 	{
-		return semestersByYearAndSemester;
+		HashMap<String, Integer> hashBlackSwan = new HashMap<String,Integer>();
 		
+		
+		return semestersByYearAndSemester;
 	}
 	
-	public int getNumCourseInNthSementer(int semester)
+	public int getNumCourseInNthSementer(int semester) // semester 은 들어오는 학기 -> HGUCoursePatternAnalyzer.java 에서 semi임
 	{
-		return semester;
+		int howMuchTaken=0;
+		int count=0;
 		
+		for (int i=0; i<coursesTaken.size();i++)
+		{
+			
+		}
+		
+		
+		return count;		
+	}
+	
+	public ArrayList<Course> getCourse()
+	{
+		return coursesTaken;
+	}
+	public String getStudentId()
+	{
+		return studentId;
 	}
 	
 }
