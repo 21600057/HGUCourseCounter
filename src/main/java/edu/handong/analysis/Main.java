47985615241079b8be28aface6c122ae3a0e7ae4
동준 <@DESKTOP-FS8K790>
2019-06-02 19:37:34 +0900

    commit

--- src/main/java/edu/handong/analysis/Main.java
+++ src/main/java/edu/handong/analysis/Main.java
@@ -10,9 +10,135 @@ import java.io.File;
 
 public class Main 
 {
+	static String inputPath;
+	static String outputPath;
+	boolean help;
+	String addOptionCk;
+	
 	public static void main(String[] args) 
 	{
-		HGUCoursePatternAnalyzer analyzer = new HGUCoursePatternAnalyzer();
-		analyzer.run(args);
+		Main main = new Main();
+		main.run(args);
+		
 	}
+	
+	public void run(String[] args)
+	{
+		
+		Options options = createOptions();
+	
+		if(parseOptions(options, args))
+		{
+			if (help)
+			{
+				printHelp(options);
+				return;
+			}
+			
+			if (addOptionCk == "1")
+			{
+				HGUCoursePatternAnalyzer analyzer = new HGUCoursePatternAnalyzer();
+				analyzer.run(args);
+			}
+			if (addOptionCk == "2")
+			{
+				
+			}
+		}	
+	}
+		
+	private boolean parseOptions(Options options, String[] args) 
+	{
+		CommandLineParser parser = new DefaultParser();
+
+		try 
+		{
+
+			CommandLine cmd = parser.parse(options, args);
+
+			inputPath = cmd.getOptionValue("i");
+			outputPath = cmd.getOptionValue("o");
+			addOptionCk = cmd.getOptionValue("a");
+			help = cmd.hasOption("h");
+			
+		} catch (Exception e) 
+		
+		{
+			printHelp(options);
+			return false;
+		}
+
+		return true;
+	}
+
+	
+	public Options createOptions() 
+	{
+		Options options = new Options();
+		
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("i").longOpt("input")
+				.desc("Set an input file path")
+				.hasArg()
+				.argName("Input path")
+				.required()
+				.build());		
+
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("o").longOpt("output")
+				.desc("Set an output file path")
+				.hasArg()
+				.argName("Output path")
+				.required()
+				.build());		
+
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("a").longOpt("analysis")
+			    .desc("1: Count courses per semester, 2: Count per course name and year")
+				.hasArg()
+				.argName("Analysis option")	
+				.required()
+				.build());		
+
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("c").longOpt("coursecode")
+			    .desc("Course code for '-a 2' option")
+				.hasArg()
+				.argName("course code")	
+				.required() // only for '-a 2'
+				.build());	
+			
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("s").longOpt("startyear")
+				.desc("Set the start year for analysis e.g., -s 2002")
+				.hasArg()
+				.argName("Start year for analysis")	
+				.required()
+				.build());	
+
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("e").longOpt("endyear")
+				.desc("Set the end year for analysis e.g., -e 2005")
+				.hasArg()
+				.argName("End year for analysis")	
+				.required()
+				.build());	
+					
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("h").longOpt("help")
+		        .desc("Show a Help page")
+				.argName("Help")	
+		        .build());
+		
+		return options;
+	}
+	
+	private void printHelp(Options options) 
+	{
+		HelpFormatter formatter = new HelpFormatter();
+		String header = "HGU Course Analyzer";
+		String footer ="";
+		formatter.printHelp("HGUCourseCounter", header, options, footer, true);
+	}
+	
 }
